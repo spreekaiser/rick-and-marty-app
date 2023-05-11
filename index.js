eadd341d5b7993ff8ec3013ef8a6cd5022b11f70
thoralf Barth <thoralfbarth@MacBook-Air-von-thoralf.local>
2023-05-11 12:28:01 +0200

    scrolling pages

--- index.js
+++ index.js
@@ -10,29 +10,45 @@ const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
-// Getting the cardObject
-const characterObject = await fetchCharacters();
-console.log(characterObject);
-
 // States
-const maxPage = 1;
-const page = 1;
+let page = 1;
 const searchQuery = "";
+
+// Getting the cardObject
+let characterObject = await fetchCharacters(page);
+const maxPage = characterObject.info.pages;
+
+console.log(characterObject);
 
 // console.log(createCharacterCard(characterObject.results[12]));
 
-async function fetchCharacters() {
-  const result = await fetch("https://rickandmortyapi.com/api/character");
+async function fetchCharacters(page) {
+  const result = await fetch(
+    `https://rickandmortyapi.com/api/character?page=${page}`
+  );
   const data = await result.json();
+  cardContainer.innerHTML = "";
+  for (let i = 0; i < data.results.length; i++) {
+    cardContainer.append(createCharacterCard(data.results[i]));
+  }
   return data;
 }
+console.log(maxPage);
 
-function getCharacterCard() {
-  cardContainer.innerHTML = "";
-  for (let i = 0; i < characterObject.results.length; i++) {
-    cardContainer.append(createCharacterCard(characterObject.results[i]));
+fetchCharacters(1);
+
+nextButton.addEventListener("click", () => {
+  if (page < maxPage) {
+    page = page + 1;
+    fetchCharacters(page);
+    pagination.innerText = `${page} / ${maxPage}`;
   }
-  console.log(cardContainer);
-}
+});
 
-getCharacterCard();
+prevButton.addEventListener("click", () => {
+  if (page >= 1) {
+    page = page - 1;
+    fetchCharacters(page);
+    pagination.innerText = `${page} / ${maxPage}`;
+  }
+});
